e28b4abae7afe400b3dd763f74241e0e9da330ea
jordanDJI <jordandjilla7@gmail.com>
2026-05-18 21:48:02 +0200

    fix pixel id meta1

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -81,7 +81,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
           t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
           document,'script','https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init','1003236108770963');fbq('track','PageView');
+          fbq('init','2019194772141718');fbq('track','PageView');
         `}</Script>
         <LanguageProvider>{children}</LanguageProvider>
       </body>
